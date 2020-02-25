9f92847fe7d64c1acfa8393c6f10128cf77e6eda
Timdavidcole <timdavidcole@gmail.com>
2020-02-25 22:56:35 +0000

    Added shuffle test

---
+++ src/shuffle.test.js
@@ -0,0 +1,9 @@
+import shuffle from './shuffle';
+
+test('shuffles an array', () => {
+  const array = [1, 2, 3]
+  const shuffledArray = shuffle(array)
+  expect(shuffledArray).toContain(1);
+  expect(shuffledArray).toContain(2);
+  expect(shuffledArray).toContain(3);
+});
