acc919f3a8f6022f1e0177d8ae2923eff9af507a
Timdavidcole <timdavidcole@gmail.com>
2020-02-26 01:38:18 +0000

    Added paw icon

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./header.css";
+import { FaPaw } from "react-icons/fa";
 
 class Header extends React.Component {
   render() {
     return (
       <header>
         <h1>
-          kit<span style={{ fontSize: "4rem" }}>Tinder</span>
+          ki<FaPaw className={"heartIcon"} />t
+          <span style={{ fontSize: "4rem" }}>Tinder</span>
         </h1>
       </header>
     );
