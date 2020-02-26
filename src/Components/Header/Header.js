dba66d7cf808a7a0345cce7e8dd53099098e6589
Timdavidcole <timdavidcole@gmail.com>
2020-02-26 02:11:46 +0000

    More css fiddling making position relative

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,24 +1,12 @@
 import React from "react";
 import "./header.css";
-import { FaPaw } from "react-icons/fa";
+import Logo from "./Logo";
 
 class Header extends React.Component {
   render() {
     return (
       <header>
-        <h1>
-          <span style={{ fontSize: "4rem" }}>k</span>i
-          <FaPaw className={"heartIcon"} />t
-          <span style={{ fontSize: "4rem" }}>T</span>
-          <div className={"inder"}>
-            <span style={{ fontSize: "2rem" }}>i</span>
-            <FaPaw
-              className={"heartIcon"}
-              style={{ left: "-1.5px", top: "16px", fontSize: "0.8rem" }}
-            />
-            <span style={{ letterSpacing: "-4px" }}>nder</span>
-          </div>
-        </h1>
+        <Logo />
       </header>
     );
   }
---
+++ src/Components/Header/Logo.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./header.css";
+import { FaPaw } from "react-icons/fa";
+
+class Logo extends React.Component {
+  render() {
+    return (
+      <h1>
+        <span style={{ fontSize: "4rem" }}>k</span>i
+        <FaPaw className={"heartIcon"} />t
+        <span style={{ fontSize: "4rem" }}>T</span>
+        <div className={"inder"}>
+          <span style={{ fontSize: "2rem" }}>i</span>
+          <FaPaw
+            className={"heartIcon"}
+            style={{ left: "-1.2%", top: "28%", fontSize: "0.8rem" }}
+          />
+          <span style={{ letterSpacing: "-4px" }}>nder</span>
+        </div>
+      </h1>
+    );
+  }
+}
+export default Logo;
